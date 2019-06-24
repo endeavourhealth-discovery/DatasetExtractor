b7be0600e9fe66daa84e5e7ec9771e506e230096
jbeaken <jack747@gmail.com>
2019-06-24 16:14:00 +0000

    Adding ReportRunner

---
+++ src/main/java/org/endeavourhealth/datasetextractor/ReportRunner.java
@@ -0,0 +1,48 @@
+package org.endeavourhealth.datasetextractor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.endeavourhealth.datasetextractor.exception.DatasetExtractorException;
+import org.endeavourhealth.datasetextractor.exception.ReportGeneratorException;
+import org.endeavourhealth.datasetextractor.repository.Repository;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.Properties;
+
+@Slf4j
+public class ReportRunner {
+
+    public static void main(String... args) throws IOException, SQLException {
+
+        Properties properties = loadProperties( args );
+
+        Repository repository = new Repository( properties );
+
+        try (  ReportGenerator reportGenerator = new ReportGenerator( properties, repository ) ) {
+
+            log.info("Starting report generator...");
+
+            reportGenerator.generate();
+
+            log.info("...all done!");
+
+        } catch (Exception e) {
+            log.error("Exception during report generator", e);
+        }
+    }
+
+    private static Properties loadProperties(String[] args) throws IOException {
+
+//        if(args.length == 0) throw new ReportGeneratorException("Required args is absent [tablename]");
+
+        Properties properties = new Properties();
+
+        InputStream inputStream = ReportRunner.class.getClassLoader().getResourceAsStream("report.generator.properties");
+
+        properties.load( inputStream );
+
+        return properties;
+    }
+
+}
