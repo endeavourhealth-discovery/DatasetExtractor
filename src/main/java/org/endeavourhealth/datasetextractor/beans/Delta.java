f6a8b13361e39b52dcc5404596b8f0ecf701f0c1
jbeaken <jack747@gmail.com>
2019-07-03 18:15:46 +0100

    Delta now checks for null in rows

--- src/main/java/org/endeavourhealth/datasetextractor/beans/Delta.java
+++ src/main/java/org/endeavourhealth/datasetextractor/beans/Delta.java
@@ -3,6 +3,7 @@ package org.endeavourhealth.datasetextractor.beans;
 import lombok.Data;
 import lombok.ToString;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -12,4 +13,13 @@ public class Delta {
 	private List<String> row;
 
 	private DeltaType type;
+
+    public void populateRow(Object[] data) {
+
+        List<String> row = new ArrayList<>();
+
+        for(Object o : data) {
+            if(o == null) row.add(""); else row.add(o.toString());
+        }
+    }
 }
