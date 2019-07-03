5c0cf80de709bdab42f782507458aba392aa5019
jbeaken <jack747@gmail.com>
2019-07-03 19:48:13 +0100

    Fixing delta returning addition as alteration

--- src/main/java/org/endeavourhealth/datasetextractor/ReportGenerator.java
+++ src/main/java/org/endeavourhealth/datasetextractor/ReportGenerator.java
@@ -1,13 +1,19 @@
 package org.endeavourhealth.datasetextractor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.endeavourhealth.datasetextractor.beans.Delta;
 import org.endeavourhealth.datasetextractor.model.Report;
 import org.endeavourhealth.datasetextractor.repository.JpaRepository;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -40,19 +46,54 @@ public class ReportGenerator implements AutoCloseable {
 
             deanonymise( report );
 
-            generateDelta( report );
+            List<Delta> deltas = generateDelta( report );
 
 //			repository.renameTable( report );
+
+            exportCsv( deltas );
 
             report.setSuccess( true );
         }
     }
 
-	private void generateDelta(Report report) {
+    private void exportCsv(List<Delta> deltas) throws IOException {
+
+        String filename = "/home/hal/test.csv";
+
+        log.info("Opening file {} for writing.....", filename);
+
+        BufferedWriter writer = Files.newBufferedWriter(Paths.get( filename ));
+
+//        fileCount++;
+
+        CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT);
+
+        for(Delta delta : deltas) {
+            csvPrinter.print( delta.getType() );
+            for(String s : delta.getRow()) {
+                csvPrinter.print( s );
+            }
+            csvPrinter.println();
+        }
+
+        csvPrinter.close( true );
+        writer.close();
+
+    }
+
+    private List<Delta> generateDelta(Report report) {
 
     	List<Delta> additions = repository.getAdditions( report );
 
-//    	List<Delta> alterations = repository.getAlterations( report );
+    	List<Delta> alterations = repository.getAlterations( report );
+
+        List<Delta> deletions = repository.getDeletions( report );
+
+        additions.addAll(alterations);
+        additions.addAll(deletions);
+
+        return additions;
+
 	}
 
 	private void callStoredProcedures(Report report) {
