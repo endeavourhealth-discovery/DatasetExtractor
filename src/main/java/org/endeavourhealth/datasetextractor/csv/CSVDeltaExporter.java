9c07ea569bc8dfc9a4c2f321b6bc482fae2440a6
jbeaken <jack747@gmail.com>
2019-07-04 21:12:55 +0100

    Upgrade to log4j 2

--- src/main/java/org/endeavourhealth/datasetextractor/csv/CSVDeltaExporter.java
+++ src/main/java/org/endeavourhealth/datasetextractor/csv/CSVDeltaExporter.java
@@ -41,9 +41,10 @@ public class CSVDeltaExporter {
             csvPrinter.println();
         }
 
-        csvPrinter.close(true);
-        writer.close();
-
     }
 
+    public void close() throws IOException {
+        csvPrinter.close(true);
+        writer.close();
+    }
 }
