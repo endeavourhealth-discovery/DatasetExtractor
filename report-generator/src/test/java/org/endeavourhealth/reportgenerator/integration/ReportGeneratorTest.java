c7bc7ff7afd7b6afd7637d5f0013a554f4e754d0
jbeaken <jack747@gmail.com>
2019-07-05 10:53:59 +0100

    Adding report generator tests

---
+++ report-generator/src/test/java/org/endeavourhealth/reportgenerator/integration/ReportGeneratorTest.java
@@ -0,0 +1,64 @@
+package org.endeavourhealth.reportgenerator.integration;
+
+import org.endeavourhealth.reportgenerator.ReportGenerator;
+import org.endeavourhealth.reportgenerator.repository.JpaRepository;
+import org.junit.*;
+import org.testcontainers.containers.GenericContainer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ReportGeneratorTest {
+
+    @ClassRule
+//    public static MySQLContainer mysql = (MySQLContainer) new MySQLContainer("mysql:5.7")
+//            .withInitScript("data_extracts.sql");
+    public static GenericContainer mysql = new GenericContainer<>("devdb-ceg")
+            .withExposedPorts(3306)
+            .withEnv("MYSQL_ROOT_PASSWORD", "admin");
+
+    private ReportGenerator reportGenerator;
+    private Properties properties;
+
+
+    @Before
+    public void init() throws Exception {
+
+        mysql.start();
+
+        String address = mysql.getContainerIpAddress();
+        Integer port = mysql.getFirstMappedPort();
+
+        properties = loadProperties();
+        properties.put("db.url", "jdbc:mysql://localhost:" + port + "/data_extracts" );
+        properties.put("db.user",  "root" );
+        properties.put("db.password",  "admin" );
+
+        JpaRepository jpaRepository = new JpaRepository(properties);
+
+        reportGenerator = new ReportGenerator(properties, jpaRepository);
+    }
+
+    @Test
+    public void getRecords() throws Exception {
+
+        reportGenerator.generate();
+
+    }
+
+    @After
+    public void after() throws Exception {
+
+    }
+
+    private static Properties loadProperties() throws IOException {
+        Properties properties = new Properties();
+
+        InputStream inputStream = ReportGeneratorTest.class.getClassLoader().getResourceAsStream("data.extractor.properties");
+
+        properties.load( inputStream );
+
+        return properties;
+    }
+}
