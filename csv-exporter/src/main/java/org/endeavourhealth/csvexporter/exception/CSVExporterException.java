7810f5660cbf9301347c1e30a38ed20c73b8a418
jbeaken <jack747@gmail.com>
2019-07-05 10:45:50 +0100

    Adding exception generic

---
+++ csv-exporter/src/main/java/org/endeavourhealth/csvexporter/exception/CSVExporterException.java
@@ -0,0 +1,7 @@
+package org.endeavourhealth.csvexporter.exception;
+
+public class CSVExporterException extends RuntimeException {
+    public CSVExporterException(String message) {
+        super(message);
+    }
+}
