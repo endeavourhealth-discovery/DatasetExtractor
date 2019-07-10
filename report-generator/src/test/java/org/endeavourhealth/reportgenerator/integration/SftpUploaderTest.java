0671f1285da9503aca2bf42df75d602f90fec0f2
jbeaken <jack747@gmail.com>
2019-07-10 11:09:24 +0100

    New integration tests

--- report-generator/src/test/java/org/endeavourhealth/reportgenerator/integration/SftpUploaderTest.java
+++ report-generator/src/test/java/org/endeavourhealth/reportgenerator/integration/SftpUploaderTest.java
@@ -1,8 +1,6 @@
 package org.endeavourhealth.reportgenerator.integration;
 
-import org.endeavourhealth.reportgenerator.ReportGenerator;
 import org.endeavourhealth.reportgenerator.model.Report;
-import org.endeavourhealth.reportgenerator.repository.JpaRepository;
 import org.endeavourhealth.reportgenerator.util.SFTPUploader;
 import org.junit.After;
 import org.junit.Before;
@@ -10,23 +8,21 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-public class SftpTest {
+public class SftpUploaderTest {
 
     @ClassRule
     public static GenericContainer sftp = new GenericContainer<>("atmoz/sftp")
             .withExposedPorts(22)
-//            .withClasspathResourceMapping("/docker/ssh_host_rsa_key",
-//                    "/etc/ssh/ssh_host_rsa_key",
-//                    BindMode.READ_ONLY)
-            .withClasspathResourceMapping("/docker/ssh_host_rsa_key.pub",
-                    "/home/foo/.ssh/keys/id_rsa.pub",
-                    BindMode.READ_ONLY)
-            .withCommand("foo::1001");
+            .withClasspathResourceMapping("/docker/ssh_host_rsa_key.pub", "/home/foo/.ssh/keys/id_rsa.pub", BindMode.READ_ONLY)
+            .withClasspathResourceMapping("/docker/sftp", "/home/foo/ftp/", BindMode.READ_WRITE)
+            .withCommand("foo::1000");
 
     private SFTPUploader sftpUploader;
 
@@ -36,7 +32,9 @@ public class SftpTest {
 
         sftp.start();
 
-        sftpUploader = new SFTPUploader();
+        Properties properties = loadProperties();
+
+        sftpUploader = new SFTPUploader( properties );
     }
 
     @Test
@@ -52,11 +50,16 @@ public class SftpTest {
         String address = sftp.getContainerIpAddress();
         Integer port = sftp.getMappedPort(22);
 
-        Report report = new Report();
+        Yaml yaml = new Yaml(new Constructor(Report.class));
+
+        InputStream yamlInputStream = this.getClass()
+                .getClassLoader()
+                .getResourceAsStream("reports.yaml");
+
+        Report report = yaml.load(yamlInputStream);
+
         report.setSftpHostname(address);
         report.setSftpPort(port);
-        report.setSftpUsername("foo");
-        report.setSftpPrivateKeyFile("/home/git/endeavour/DatasetExtractor/report-generator/src/test/etc/docker/ssh_host_rsa_key");
 
         return report;
     }
@@ -69,9 +72,9 @@ public class SftpTest {
     private static Properties loadProperties() throws IOException {
         Properties properties = new Properties();
 
-        InputStream inputStream = SftpTest.class.getClassLoader().getResourceAsStream("report.generator.properties");
+        InputStream inputStream = SftpUploaderTest.class.getClassLoader().getResourceAsStream("report.generator.properties");
 
-        properties.load( inputStream );
+        properties.load(inputStream);
 
         return properties;
     }
