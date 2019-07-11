dc3fd3112b2fe4f3bf40449b6b052c2e4240f130
jbeaken <jack747@gmail.com>
2019-07-11 10:11:48 +0100

    Fixing test

--- report-generator/src/test/java/org/endeavourhealth/reportgenerator/integration/ReportGeneratorTest.java
+++ report-generator/src/test/java/org/endeavourhealth/reportgenerator/integration/ReportGeneratorTest.java
@@ -2,18 +2,25 @@ package org.endeavourhealth.reportgenerator.integration;
 
 import org.endeavourhealth.reportgenerator.ReportGenerator;
 import org.endeavourhealth.reportgenerator.repository.JpaRepository;
+import org.endeavourhealth.reportgenerator.util.SFTPUploader;
 import org.junit.*;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+import org.mockito.Mockito;
+import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-public class ReportGeneratorTest {
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+public class ReportGeneratorTest extends AbstractTest {
 
     @ClassRule
-//    public static MySQLContainer mysql = (MySQLContainer) new MySQLContainer("mysql:5.7")
-//            .withInitScript("data_extracts.sql");
+//    public static MySQLContainer mysql = (MySQLContainer) new MySQLContainer("mysql:5.7").withInitScript("data_extracts.sql");
     public static GenericContainer mysql = new GenericContainer<>("devdb-ceg")
             .withExposedPorts(3306)
             .withEnv("MYSQL_ROOT_PASSWORD", "admin");
@@ -32,36 +39,21 @@ public class ReportGeneratorTest {
 
         properties = loadProperties();
         properties.put("db.core.url", "jdbc:mysql://localhost:" + port + "/data_extracts" );
-        properties.put("db.core.user",  "root" );
-        properties.put("db.core.password",  "admin" );
+        properties.put("db.core.user", "root" );
+        properties.put("db.core.password", "admin" );
         properties.put("db.compass.url", "jdbc:mysql://localhost:" + port + "/data_extracts" );
-        properties.put("db.compass.user",  "root" );
-        properties.put("db.compass.password",  "admin" );
+        properties.put("db.compass.user", "root" );
+        properties.put("db.compass.password", "admin" );
 
-        JpaRepository jpaRepository = new JpaRepository(properties);
+        SFTPUploader sftpUploader = Mockito.mock(SFTPUploader.class);
 
-        reportGenerator = new ReportGenerator(properties, jpaRepository);
+        doNothing().when(sftpUploader);
+
+        reportGenerator = new ReportGenerator(properties, sftpUploader);
     }
 
     @Test
     public void getRecords() throws Exception {
-
         reportGenerator.generate();
-
-    }
-
-    @After
-    public void after() throws Exception {
-
-    }
-
-    private static Properties loadProperties() throws IOException {
-        Properties properties = new Properties();
-
-        InputStream inputStream = ReportGeneratorTest.class.getClassLoader().getResourceAsStream("report.generator.properties");
-
-        properties.load( inputStream );
-
-        return properties;
     }
 }
