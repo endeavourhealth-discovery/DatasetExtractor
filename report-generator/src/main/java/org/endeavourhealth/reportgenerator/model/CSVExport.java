f486dd1670c774c015e1757500b3753b29ed01fe
jbeaken <jack747@gmail.com>
2019-08-23 12:42:49 +0100

    Adding database to csv export

--- report-generator/src/main/java/org/endeavourhealth/reportgenerator/model/CSVExport.java
+++ report-generator/src/main/java/org/endeavourhealth/reportgenerator/model/CSVExport.java
@@ -1,18 +1,25 @@
 package org.endeavourhealth.reportgenerator.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
+import lombok.AllArgsConstructor;
 
 import java.util.List;
 
+
 @Data
 @ToString
+@AllArgsConstructor
 public class CSVExport {
 
-    private List<Table> csvTablesToExport;
-    private String outputDirectory;
-    private Database database = Database.COMPASS;
-    private Integer maxNumOfRowsInEachOutputFile = 0;  //0 is no limit
-    private Boolean switchedOn = true;
+    private List<Table> tables;
 
+    private String outputDirectory;
+
+    private Database database = Database.COMPASS;
+
+    private Integer maxNumOfRowsInEachOutputFile = 0;  //0 is no limit
+
+    private Boolean switchedOn = true;
 }
