e1c57822727fb9ef751c379df261d3ef2d336a9f
jbeaken <jack747@gmail.com>
2019-08-28 12:46:07 +0100

    Added switch for delta and deanonymise

--- report-generator/src/main/java/org/endeavourhealth/reportgenerator/util/ExtensionExecutor.java
+++ report-generator/src/main/java/org/endeavourhealth/reportgenerator/util/ExtensionExecutor.java
@@ -5,5 +5,20 @@ import org.endeavourhealth.reportgenerator.model.Extension;
 public class ExtensionExecutor {
 
     public void execute(Extension extension) {
+
+        switch(extension.getType()) {
+            case DELTA:
+                executeDelta( extension );
+                break;
+            case DEANONYMISE:
+                executeDeanonymise( extension );
+            }
+        }
+
+    private void executeDeanonymise(Extension extension) {
+    }
+
+    private void executeDelta(Extension extension) {
     }
 }
+
