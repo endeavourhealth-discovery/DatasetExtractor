83fcd6256126e064ba132f934b3f1c338af939c8
jbeaken <jack747@gmail.com>
2019-08-28 12:34:47 +0100

    Class ExtensionExecutor

---
+++ report-generator/src/main/java/org/endeavourhealth/reportgenerator/util/ExtensionExecutor.java
@@ -0,0 +1,9 @@
+package org.endeavourhealth.reportgenerator.util;
+
+import org.endeavourhealth.reportgenerator.model.Extension;
+
+public class ExtensionExecutor {
+
+    public void execute(Extension extension) {
+    }
+}
