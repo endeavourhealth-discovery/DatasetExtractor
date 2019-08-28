a9df7d2c38af46f084cc4c5268358ed22d4b3666
jbeaken <jack747@gmail.com>
2019-08-28 13:06:17 +0100

    Adding properties to extension

--- report-generator/src/main/java/org/endeavourhealth/reportgenerator/model/Extension.java
+++ report-generator/src/main/java/org/endeavourhealth/reportgenerator/model/Extension.java
@@ -2,14 +2,16 @@ package org.endeavourhealth.reportgenerator.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Extension {
 
     private ExtensionType type;
-    private Map<String, String> properties;
 
+    private Map<String, String> properties;
 }
