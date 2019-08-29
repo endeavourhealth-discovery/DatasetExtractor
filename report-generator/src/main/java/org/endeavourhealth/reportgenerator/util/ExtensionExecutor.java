9c2a2d373956a95c96d7403454c6c4e38fd905be
jbeaken <jack747@gmail.com>
2019-08-29 13:13:04 +0100

    Adding executeDeanonymise method

--- report-generator/src/main/java/org/endeavourhealth/reportgenerator/util/ExtensionExecutor.java
+++ report-generator/src/main/java/org/endeavourhealth/reportgenerator/util/ExtensionExecutor.java
@@ -3,17 +3,25 @@ package org.endeavourhealth.reportgenerator.util;
 import lombok.extern.slf4j.Slf4j;
 import org.endeavourhealth.reportgenerator.model.Extension;
 import org.endeavourhealth.reportgenerator.model.ExtensionType;
+import org.endeavourhealth.reportgenerator.model.Report;
+import org.endeavourhealth.reportgenerator.repository.JpaRepository;
+
+import java.util.List;
 
 @Slf4j
 public class ExtensionExecutor {
+
+    private final JpaRepository repository;
+
+    public ExtensionExecutor(JpaRepository repository) {
+        this.repository = repository;
+    }
 
     public void execute(Extension extension) {
 
         ExtensionType type = extension.getType();
 
-        log.info("Executing extension {}", type.getDisplayName());
-
-        log.info("with properties {}", extension.getProperties());
+        log.info("Executing extension {}", extension);
 
         switch( type ) {
             case DELTA:
@@ -25,6 +33,25 @@ public class ExtensionExecutor {
         }
 
     private void executeDeanonymise(Extension extension) {
+
+            log.info("Report required deanonymising, running...");
+
+            repository.bootEntityManagerFactoryCore();
+
+            Integer offset = 0;
+
+            List<String> pseudoIds = repository.getPseudoIds(offset);
+
+            while (pseudoIds.size() > 0) {
+
+                repository.deanonymise(pseudoIds);
+
+                offset += 3000;
+
+                pseudoIds = repository.getPseudoIds(offset);
+            }
+
+            log.info("...deanonymising all done");
     }
 
     private void executeDelta(Extension extension) {
