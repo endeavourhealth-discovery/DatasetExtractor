283d989ede1ef68afaa0196da83696f4082bc01c
jbeaken <jack747@gmail.com>
2019-08-29 13:12:46 +0100

    Adding @ToString

--- report-generator/src/main/java/org/endeavourhealth/reportgenerator/model/Extension.java
+++ report-generator/src/main/java/org/endeavourhealth/reportgenerator/model/Extension.java
@@ -3,12 +3,14 @@ package org.endeavourhealth.reportgenerator.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.Map;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Extension {
 
     private ExtensionType type;
