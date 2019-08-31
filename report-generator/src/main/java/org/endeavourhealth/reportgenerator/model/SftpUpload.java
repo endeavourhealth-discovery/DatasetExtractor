8ca530faa9d1cc42b458efff15e02c60cc241615
jbeaken <jack747@gmail.com>
2019-08-31 10:30:56 +0100

    Sftp bean validation

--- report-generator/src/main/java/org/endeavourhealth/reportgenerator/model/SftpUpload.java
+++ report-generator/src/main/java/org/endeavourhealth/reportgenerator/model/SftpUpload.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 
@@ -14,14 +15,18 @@ import java.util.List;
 @NoArgsConstructor
 public class SftpUpload {
 
+    @NotNull
     private String privateKeyFile;
 
+    @NotNull
     private String hostname;
 
+    @NotNull
     private String username;
 
-    private Integer port;
+    private Integer port = 22;
 
+    @NotNull
     private String hostDirectory;
 
     private Boolean switchedOn = true;
