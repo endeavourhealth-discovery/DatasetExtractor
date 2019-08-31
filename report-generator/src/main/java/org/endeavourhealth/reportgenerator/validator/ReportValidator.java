0d9cf78379e4a4c024279aa955fc0dab3eacdb84
jbeaken <jack747@gmail.com>
2019-08-31 10:30:26 +0100

    Validator for reports using hibernate validator

---
+++ report-generator/src/main/java/org/endeavourhealth/reportgenerator/validator/ReportValidator.java
@@ -0,0 +1,38 @@
+package org.endeavourhealth.reportgenerator.validator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.endeavourhealth.reportgenerator.model.Report;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+
+@Slf4j
+public class ReportValidator {
+
+    private final Validator validator;
+
+    public ReportValidator() {
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    public Set<ConstraintViolation<Report>> validate(Report report) {
+
+        Set<ConstraintViolation<Report>> constraintViolations = validator.validate( report );
+
+        report.setConstraintViolations( constraintViolations );
+
+        if(!constraintViolations.isEmpty()) {
+            log.warn("Report has failed validations {}", constraintViolations);
+            report.setIsValid( false );
+
+        }
+
+        return validator.validate( report );
+    }
+}
