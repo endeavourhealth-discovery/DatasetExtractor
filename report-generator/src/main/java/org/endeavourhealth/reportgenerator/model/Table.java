5902f17f0a6646878be22a2f4ede316205ba4642
jbeaken <jack747@gmail.com>
2019-09-01 09:30:41 +0100

    Validation on tables with messages

--- report-generator/src/main/java/org/endeavourhealth/reportgenerator/model/Table.java
+++ report-generator/src/main/java/org/endeavourhealth/reportgenerator/model/Table.java
@@ -3,12 +3,17 @@ package org.endeavourhealth.reportgenerator.model;
 import lombok.Data;
 import lombok.ToString;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 
 @ToString
 @Data
 public class Table {
+
+    @NotNull(message = "CSV Table name cannot be null")
     private String name;
+
+    @NotNull(message = "CSV Filename cannot be null")
     private String fileName;
 }
