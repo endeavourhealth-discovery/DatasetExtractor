0e310ef0e5e544a41fdabfb688cabef7263efd03
jbeaken <jack747@gmail.com>
2019-09-04 17:29:00 +0100

    Adding lombok @Data etc

--- report-generator/src/main/java/org/endeavourhealth/reportgenerator/model/Delta.java
+++ report-generator/src/main/java/org/endeavourhealth/reportgenerator/model/Delta.java
@@ -1,9 +1,16 @@
 package org.endeavourhealth.reportgenerator.model;
 
+import lombok.Data;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
+@Data
+@ToString
+@Slf4j
 public class Delta {
 
     @NotEmpty(message = "Delta must have at least one table to export")
