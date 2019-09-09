b9a53b6ad778ff2ed82503cda7cbd8594d35eff1
jbeaken <jack747@gmail.com>
2019-09-09 18:34:38 +0100

    Adding fihr module to report generator project

---
+++ fihr-exporter/src/main/java/org/endeavourhealth/fihrexporter/FihrExporter.java
@@ -0,0 +1,153 @@
+package org.endeavourhealth.fihrexporter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.QuoteMode;
+import org.endeavourhealth.fihrexporter.repository.Repository;
+
+import java.io.BufferedWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Arrays;
+import java.util.Properties;
+
+@Slf4j
+public class FihrExporter implements AutoCloseable {
+
+    private final Repository repository;
+
+    private BufferedWriter writer;
+
+    private CSVPrinter csvPrinter;
+
+    private final String outputDirectory;
+
+    private int fileCount = 0;
+
+    private final int noOfRowsInEachOutputFile;
+
+    private final int noOfRowsInEachDatabaseFetch;
+
+    private final int pageSize;
+
+    private final String dbTableName;
+
+    private final String csvFilename;
+
+    public FihrExporter(Properties properties) throws Exception {
+        this(properties, new Repository(properties));
+    }
+
+    public FihrExporter(Properties properties, Repository repository) {
+
+        this.repository = repository;
+
+        log.info("**** Booting CSVExporter, loading property file and db repository.....");
+
+        outputDirectory = properties.getProperty("outputDirectory");
+
+        csvFilename = buildCsvFilename( properties.getProperty("csvFilename") );
+
+        dbTableName = properties.getProperty("dbTableName");
+
+        noOfRowsInEachOutputFile = Integer.valueOf( properties.getProperty("noOfRowsInEachOutputFile") );
+
+        noOfRowsInEachDatabaseFetch =  Integer.valueOf( properties.getProperty("noOfRowsInEachDatabaseFetch") );
+
+        if(noOfRowsInEachOutputFile > 0) {
+          pageSize = noOfRowsInEachOutputFile < noOfRowsInEachDatabaseFetch ? noOfRowsInEachOutputFile : noOfRowsInEachDatabaseFetch;
+        } else {
+          pageSize = noOfRowsInEachDatabaseFetch;
+        }
+
+        log.info("Exporting db table {} to file {} to directory", dbTableName, csvFilename, outputDirectory);
+
+        log.info("noOfRowsInEachDatabaseFetch = {}", noOfRowsInEachDatabaseFetch);
+        log.info("noOfRowsInEachOutputFile = {}", noOfRowsInEachOutputFile);
+
+        log.info("**** CSVExporter successfully booted!!");
+    }
+
+    private String buildCsvFilename(String csvFilename) {
+        if(csvFilename.contains("{today}")) {
+
+            LocalDate localDate = LocalDate.now();
+
+            String today = localDate.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+
+            return  csvFilename.replace("{today}", today);
+        }
+
+        return csvFilename;
+    }
+
+    public void exportCSV() throws Exception {
+
+        fileCount = 0;
+
+        bootNewPrintWriter();
+
+        int currentFileCount = 0, offset = 0;
+
+        List<List<String>> result = repository.getRows(offset, pageSize);
+
+        while(result.size() > 0) {
+
+            csvPrinter.printRecords( result );
+
+            offset += result.size();
+
+            currentFileCount += result.size();
+
+            //noOfRowsInEachOutputFile == 0 or smaller, no limit
+            if(currentFileCount > noOfRowsInEachOutputFile && noOfRowsInEachOutputFile > 0) {
+
+                csvPrinter.close( true );
+
+                writer.close();
+
+                bootNewPrintWriter();
+
+                currentFileCount = 0;
+            }
+
+            result = repository.getRows(offset, pageSize);
+
+            log.info("No of rows processed {}", offset);
+        }
+
+        log.info("Finished writing csv");
+    }
+
+    private void bootNewPrintWriter() throws Exception {
+
+        String outputFileName = fileCount == 0 ?  outputDirectory + csvFilename + ".csv" : outputDirectory  + csvFilename + fileCount + ".csv";
+
+        log.info("Opening file {} for writing.....", outputFileName);
+
+        String[] headers = repository.getHeaders();
+
+        log.debug("With headers {}", Arrays.toString( headers ));
+
+        writer = Files.newBufferedWriter(Paths.get( outputFileName ));
+
+        fileCount++;
+
+        csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader( headers ).withQuoteMode(QuoteMode.ALL_NON_NULL));
+    }
+
+
+
+    @Override
+    public void close() throws Exception {
+        csvPrinter.close( true );
+
+        writer.close();
+
+        repository.close();
+    }
+}
---
+++ fihr-exporter/src/main/java/org/endeavourhealth/fihrexporter/repository/Repository.java
@@ -0,0 +1,114 @@
+package org.endeavourhealth.fihrexporter.repository;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+@Slf4j
+public class Repository {
+
+    private MysqlDataSource dataSource;
+
+    private Connection connection;
+
+    private String tableName;
+    private String orderBy;
+
+    private String[] headers;
+    private int columnCount;
+
+    public Repository(Properties properties) throws SQLException {
+        init( properties );
+    }
+
+    public String[] getHeaders() throws SQLException {
+        return headers;
+    }
+
+    private void initTableHeaders() throws SQLException {
+
+        String preparedSql = getSql() + " limit 1";
+
+        PreparedStatement preparedStatement = connection.prepareStatement( preparedSql );
+
+        ResultSet rs = preparedStatement.executeQuery();
+
+        ResultSetMetaData metaData = rs.getMetaData();
+
+        columnCount = metaData.getColumnCount();
+
+        String[] labels = null;
+        if (metaData != null) {
+            labels = new String[columnCount];
+            for (int i = 0; i < columnCount; i++) {
+                labels[i] = metaData.getColumnLabel(i + 1);
+            }
+        }
+
+        preparedStatement.close();
+
+        headers = labels;
+    }
+
+    public List<List<String>> getRows(int offset, int pageSize) throws SQLException {
+
+        String preparedSql = getSql() + " limit " + offset + ", " + pageSize;
+
+        PreparedStatement preparedStatement = connection.prepareStatement( preparedSql );
+
+        ResultSet rs = preparedStatement.executeQuery();
+
+        List<List<String>> result = new ArrayList<>();
+
+        while (rs.next()) {
+
+            List<String> row = new ArrayList<>();
+
+            for (int i = 1; i <= columnCount; i++) {
+                row.add( rs.getString( i ));
+            }
+
+            result.add( row );
+        }
+
+        preparedStatement.close();
+
+        return result;
+    }
+
+    private String whitelist(String tableName) {
+        return tableName.replaceAll("/[^0-9a-zA-Z_]/", "");
+    }
+
+
+    private void init(Properties props) throws SQLException {
+
+        dataSource = new MysqlDataSource();
+
+        dataSource.setURL(props.getProperty("url"));
+        dataSource.setUser(props.getProperty("user"));
+        dataSource.setPassword(props.getProperty("password"));
+
+        dataSource.setReadOnlyPropagatesToServer(true);
+
+        connection = dataSource.getConnection();
+
+        tableName = props.getProperty("dbTableName");
+
+        connection.setReadOnly( true );
+
+        initTableHeaders();
+    }
+
+    private String getSql() {
+        return "select * from " + tableName + " order by " + orderBy;
+    }
+
+    public void close() throws SQLException {
+        connection.close();
+    }
+}
