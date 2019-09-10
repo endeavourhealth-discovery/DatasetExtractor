29d48ba86282e92f76da850de670c84b822984cd
jbeaken <jack747@gmail.com>
2019-09-10 12:27:54 +0100

    Latest fihr stuff

--- fihr-exporter/src/main/java/org/endeavourhealth/fihrexporter/FihrExporterRunner.java
+++ fihr-exporter/src/main/java/org/endeavourhealth/fihrexporter/FihrExporterRunner.java
@@ -1,14 +1,10 @@
 package org.endeavourhealth.fihrexporter;
 
-import lombok.extern.slf4j.Slf4j;
-import org.endeavourhealth.fihrexporter.exception.FihrExporterException;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.Properties;
 
-@Slf4j
 public class FihrExporterRunner {
 
     public static void main(String... args) throws IOException, SQLException {
@@ -17,31 +13,20 @@ public class FihrExporterRunner {
 
         try (  FihrExporter csvExporter = new FihrExporter( properties  ) ) {
 
-            log.info("Starting fihr exporter...");
-
-            csvExporter.exportCSV();
-
-            log.info("...all done!");
+            csvExporter.export();
 
         } catch (Exception e) {
-            log.error("Exception during export", e);
+            //do something!
         }
     }
 
     private static Properties loadProperties(String[] args) throws IOException {
 
-        if(args.length == 0) throw new FihrExporterException("Required args is absent [tablename]");
-
         Properties properties = new Properties();
 
-        InputStream inputStream = FihrExporterRunner.class.getClassLoader().getResourceAsStream("csv.exporter.properties");
+        InputStream inputStream = FihrExporterRunner.class.getClassLoader().getResourceAsStream("fihr.exporter.properties");
 
         properties.load( inputStream );
-
-        properties.put("dbTableName", args[0]);
-        properties.put("csvFilename", args[0]);
-
-        if(args.length > 1) properties.put("orderBy", args[1]);
 
         return properties;
     }
--- fihr-exporter/src/main/java/org/endeavourhealth/fihrexporter/repository/Repository.java
+++ fihr-exporter/src/main/java/org/endeavourhealth/fihrexporter/repository/Repository.java
@@ -1,62 +1,25 @@
 package org.endeavourhealth.fihrexporter.repository;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
-import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-@Slf4j
 public class Repository {
 
     private MysqlDataSource dataSource;
 
     private Connection connection;
 
-    private String tableName;
-    private String orderBy;
-
-    private String[] headers;
-    private int columnCount;
-
     public Repository(Properties properties) throws SQLException {
         init( properties );
     }
 
-    public String[] getHeaders() throws SQLException {
-        return headers;
-    }
-
-    private void initTableHeaders() throws SQLException {
-
-        String preparedSql = getSql() + " limit 1";
-
-        PreparedStatement preparedStatement = connection.prepareStatement( preparedSql );
-
-        ResultSet rs = preparedStatement.executeQuery();
-
-        ResultSetMetaData metaData = rs.getMetaData();
-
-        columnCount = metaData.getColumnCount();
-
-        String[] labels = null;
-        if (metaData != null) {
-            labels = new String[columnCount];
-            for (int i = 0; i < columnCount; i++) {
-                labels[i] = metaData.getColumnLabel(i + 1);
-            }
-        }
-
-        preparedStatement.close();
-
-        headers = labels;
-    }
-
     public List<List<String>> getRows(int offset, int pageSize) throws SQLException {
 
-        String preparedSql = getSql() + " limit " + offset + ", " + pageSize;
+        String preparedSql = "select * from table";
 
         PreparedStatement preparedStatement = connection.prepareStatement( preparedSql );
 
@@ -66,24 +29,12 @@ public class Repository {
 
         while (rs.next()) {
 
-            List<String> row = new ArrayList<>();
-
-            for (int i = 1; i <= columnCount; i++) {
-                row.add( rs.getString( i ));
-            }
-
-            result.add( row );
         }
 
         preparedStatement.close();
 
         return result;
     }
-
-    private String whitelist(String tableName) {
-        return tableName.replaceAll("/[^0-9a-zA-Z_]/", "");
-    }
-
 
     private void init(Properties props) throws SQLException {
 
@@ -97,16 +48,10 @@ public class Repository {
 
         connection = dataSource.getConnection();
 
-        tableName = props.getProperty("dbTableName");
+        connection.setReadOnly(true);
 
-        connection.setReadOnly( true );
-
-        initTableHeaders();
     }
 
-    private String getSql() {
-        return "select * from " + tableName + " order by " + orderBy;
-    }
 
     public void close() throws SQLException {
         connection.close();
