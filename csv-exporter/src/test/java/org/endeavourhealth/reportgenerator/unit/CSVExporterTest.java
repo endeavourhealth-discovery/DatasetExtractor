e175ebec3e3a28d3721ac558f6bacb55a5262761
Paul Simon <psimon1@supanet.com>
2019-09-18 14:53:34 +0100

    Revert "Address line fix"
    This reverts commit d85ec9ed758f2e9fd4665c07363bf32677750a85.

--- csv-exporter/src/test/java/org/endeavourhealth/reportgenerator/unit/CSVExporterTest.java
+++ csv-exporter/src/test/java/org/endeavourhealth/reportgenerator/unit/CSVExporterTest.java
@@ -89,7 +89,7 @@ public class CSVExporterTest {
     private static Properties loadProperties() throws IOException {
         Properties properties = new Properties();
 
-        InputStream inputStream = CSVExporterRunner.class.getClassLoader().getResourceAsStream("csv.exporter.properties.example");
+        InputStream inputStream = CSVExporterRunner.class.getClassLoader().getResourceAsStream("csv.exporter.properties");
 
         properties.load( inputStream );
 
