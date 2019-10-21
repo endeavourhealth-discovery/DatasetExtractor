cf8998535e477cfda2fa0f14e91880bf08fef331
jbeaken <jack747@gmail.com>
2019-10-21 14:36:51 +0100

    Adding slack reporter

---
+++ report-generator/src/main/java/org/endeavourhealth/reportgenerator/slack/SlackReporter.java
@@ -0,0 +1,14 @@
+package org.endeavourhealth.reportgenerator.slack;
+
+import org.endeavourhealth.reportgenerator.model.Report;
+
+import java.util.List;
+
+public class SlackReporter {
+
+
+    public void report(List<Report> reports) {
+
+
+    }
+}
