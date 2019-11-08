3f2bdb650ca8de726c820fb66d53753bc2d0f561
jbeaken <jack747@gmail.com>
2019-11-08 12:35:43 +0000

    Added tests for new zipper functionality

--- report-generator/src/test/java/org/endeavourhealth/reportgenerator/unit/FileZipperTest.java
+++ report-generator/src/test/java/org/endeavourhealth/reportgenerator/unit/FileZipperTest.java
@@ -1,18 +1,16 @@
 package org.endeavourhealth.reportgenerator.unit;
 
-import org.endeavourhealth.reportgenerator.ReportGenerator;
 import org.endeavourhealth.reportgenerator.model.CSVExport;
 import org.endeavourhealth.reportgenerator.model.Report;
 import org.endeavourhealth.reportgenerator.model.SftpUpload;
+import org.endeavourhealth.reportgenerator.model.Zipper;
 import org.endeavourhealth.reportgenerator.util.FileZipper;
+import org.jetbrains.annotations.NotNull;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Properties;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,7 +18,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class FileZipperTest extends AbstractTest {
 
 
-    private Report report;
     private Properties properties;
     private static File zipFile;
 
@@ -28,21 +25,27 @@ public class FileZipperTest extends AbstractTest {
     public void setUp() throws Exception {
         properties = loadProperties();
 
-        report = new Report();
-
-        SftpUpload sftpUpload = new SftpUpload();
-        sftpUpload.setZipFilename("zipit_{today}");
-
-        CSVExport csvExport = new CSVExport();
-        csvExport.setOutputDirectory("/home/hal/dev/data_extracts/file_zipper/");
-
-        report.setSftpUpload(sftpUpload);
-        report.setCsvExport(csvExport);
-
+        properties.put("csv.staging.directory", "/home/hal/dev/data_extracts/file_zipper_output/");
     }
 
-    @AfterClass
-    public static void tearDown() throws Exception {
+    @NotNull
+    private Report getReport() {
+        Report report = new Report();
+
+        SftpUpload sftpUpload = new SftpUpload();
+
+        Zipper zipper = new Zipper();
+        zipper.setZipFilename("zipit_{today}");
+        zipper.setSourceDirectory("/home/hal/dev/data_extracts/file_zipper/");
+
+        report.setSftpUpload(sftpUpload);
+        report.setZipper(zipper);
+
+        return report;
+    }
+
+    @After
+    public void tearDown() throws Exception {
         if(zipFile != null) {
             zipFile.delete();
         }
@@ -52,7 +55,50 @@ public class FileZipperTest extends AbstractTest {
     @Test
     public void perfectRun() throws Exception {
 
-        properties.put("csv.staging.directory", "/home/hal/dev/data_extracts/file_zipper_output/");
+        Report report = getReport();
+
+        FileZipper fileZipper = new FileZipper(report, properties);
+
+        String filePath = fileZipper.zip();
+
+        zipFile = new File(filePath);
+
+        assertThat( zipFile  )
+                .exists()
+                .isFile();
+
+    }
+
+
+    @Test
+    public void nullZipper() throws Exception {
+
+        Report report = getReport();
+
+        //Must have csv export output directory set if zipper is null
+        CSVExport csvExport = new CSVExport();
+        csvExport.setOutputDirectory("/home/hal/dev/data_extracts/file_zipper/");
+
+        report.setZipper(null);
+        report.setCsvExport(csvExport);
+
+        FileZipper fileZipper = new FileZipper(report, properties);
+
+        String filePath = fileZipper.zip();
+
+        zipFile = new File(filePath);
+
+        assertThat( zipFile  )
+                .exists()
+                .isFile();
+
+    }
+
+    @Test
+    public void splitFiles() throws Exception {
+
+        Report report = getReport();
+        report.getZipper().setSplitFiles(true);
 
         FileZipper fileZipper = new FileZipper(report, properties);
 
