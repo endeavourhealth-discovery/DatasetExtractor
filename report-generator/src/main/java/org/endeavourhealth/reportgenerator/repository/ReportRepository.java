97d5dcb35a2d4db0cbc3380c3136e2a3d5bf4c73
Jack Beaken <jack747@gmail.com>
2019-11-29 14:35:33 +0000

    Commented out assembly plugin, due to clash of persistence.xml files

--- report-generator/src/main/java/org/endeavourhealth/reportgenerator/repository/ReportRepository.java
+++ report-generator/src/main/java/org/endeavourhealth/reportgenerator/repository/ReportRepository.java
@@ -1,14 +1,21 @@
 package org.endeavourhealth.reportgenerator.repository;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.endeavourhealth.reportgenerator.model.Report;
+import org.hibernate.jpa.boot.internal.EntityManagerFactoryBuilderImpl;
+import org.hibernate.jpa.boot.internal.PersistenceUnitInfoDescriptor;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.spi.PersistenceUnitInfo;
+import javax.sql.DataSource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
-public class ReportRepository implements AutoCloseable {
+public class ReportRepository extends Repository implements AutoCloseable {
 
     private  EntityManagerFactory entityManagerFactory;
 
@@ -17,14 +24,15 @@ public class ReportRepository implements AutoCloseable {
     }
 
     public ReportRepository(Properties properties) {
-
         this();
 
         properties.put("javax.persistence.jdbc.password", properties.get("db.report.password"));
         properties.put("javax.persistence.jdbc.user", properties.getProperty("db.report.user"));
         properties.put("javax.persistence.jdbc.url", properties.getProperty("db.report.url"));
 
-        entityManagerFactory = Persistence.createEntityManagerFactory("reportDatabase", properties);
+        List<Class> managedClasses = List.of(Report.class);Á
+
+        entityManagerFactory = getEntityManagerFactory( getClass().getSimpleName(), properties);
     }
 
     public void save(List<Report> reports) {
