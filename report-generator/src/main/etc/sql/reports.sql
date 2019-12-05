cfb97573febba67aacc0be798b944523ca5a06f8
jbeaken <jack747@gmail.com>
2019-12-05 16:53:34 +0000

    Adding use data_extracts

--- report-generator/src/main/etc/sql/reports.sql
+++ report-generator/src/main/etc/sql/reports.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `CSVExport`
 --
 
+use data_extracts;
+
 DROP TABLE IF EXISTS `CSVExport`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
